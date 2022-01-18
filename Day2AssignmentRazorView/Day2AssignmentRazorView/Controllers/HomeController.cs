90429ec171a78db919541caaca045becb717a7fa
AmrutaBIT <amrutadesai@benchmarkit.solutions>
2022-01-18 15:17:08 +0530

    Day2 Work

---
+++ Day2AssignmentRazorView/Day2AssignmentRazorView/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day2AssignmentRazorView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int amount)
+        {
+            ViewBag.amount=amount;
+            return View();
+        }
+    }
+}
