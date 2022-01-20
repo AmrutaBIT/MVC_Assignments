be9585e414deb83f6e30afcd8463e6bd18ded6df
AmrutaBIT <amrutadesai@benchmarkit.solutions>
2022-01-20 16:41:09 +0530

    Day4 Work

---
+++ Day4RoutingDemo/Day4RoutingDemo/App_Start/RouteConfig.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Day4RoutingDemo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //Using this only one of the routes work so we modify it as below
+            //routes.MapRoute(
+            //    name: "Product",
+            //    url: "{controller}/{action}/{prodName}",
+            //    defaults: new { }
+            //    );
+
+            //Method1
+            //    routes.MapRoute(
+            //    name: "Products",
+            //    url: "Product/getprodid/{prodName}",
+            //    defaults: new { controller = "Product", action = "GetProdId" }
+            //    );
+
+            //Method2 using Contraints
+            routes.MapRoute(
+                name: "Products",
+                url: "{controller}/{action}/{prodName}",
+                defaults: new { },
+                constraints: new { prodName = @"^[A-Za-z]*$" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Day4RoutingDemo/Day4RoutingDemo/Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day4RoutingDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Product/Details/{id:int}")]
+        public ActionResult Details(int? id)
+        {
+            var product = new[] {
+            new {productid=1, productname="Iphone",cost=80000},
+            new { productid = 2, productname = "VivoY12", cost = 80000 },
+            new { productid = 3, productname = "SamsungA4", cost = 80000 },
+        };
+            string productname = "";
+            if (id == null)
+            {
+                return Content("Please pass a product id");
+            }
+            else
+            {
+                foreach (var pro in product)
+                {
+                    if (pro.productid == id)
+                    {
+                        productname = pro.productname;
+                    }
+
+                }
+                return Content(productname);
+            }
+        
+        }
+        [Route("Product/GetProdId/{prodName:string}")]
+        public ActionResult GetProdId(string prodName)
+        {
+            var product = new[] {
+            new {productid=1, productname="Iphone",cost=80000},
+            new { productid = 2, productname = "VivoY12", cost = 80000 },
+            new { productid = 3, productname = "SamsungA4", cost = 80000 },
+        };
+            int productid = 0;
+            if (prodName == null)
+            {
+                return Content("Please enter a product Name");
+            }
+            else
+            {
+                foreach (var item in product)
+                {
+                    if (item.productname == prodName)
+                    {
+                        productid = item.productid;
+                    }
+                }
+                return Content(productid.ToString());
+            }
+
+        }
+
+    }
+}
